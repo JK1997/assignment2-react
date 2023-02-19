ba0df5a1c59fcb59e2aa3c994621a72e73bef870
Ong-Jun-Kai <ongjunkai@ecquaria.com>
2023-02-19 15:21:02 +0800

    Set up a successful and fail upload alert message

--- src/dashboard/UploadFile.js
+++ src/dashboard/UploadFile.js
@@ -1,36 +1,54 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import {Alert, Stack} from "@mui/material";
 
 const UploadFile = () => {
     // a local state to store the currently selected file.
     const [selectedFile, setSelectedFile] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [message, setMessage] = useState("");
+    const [isError, setIsError] = useState(false);
 
-    const handleSubmit = async(event) => {
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0])
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault()
 
         const formData = new FormData();
         if (selectedFile) {
             formData.append("file", selectedFile);
         }
-        console.log(selectedFile);
+
+        setProgress(0);
 
         try {
             const response = await axios.post('http://localhost:8080/uploads', formData);
             console.log(response);
+            setMessage("Upload file successfully");
+            setSelectedFile(undefined);
+            setIsError(false);
         } catch (error) {
             console.log(error);
+            setProgress(0);
+            setMessage("Could not upload the file!");
+            setIsError(true);
         }
     }
 
-    const handleFileSelect = (event) => {
-        setSelectedFile(event.target.files[0])
-    }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="file" onChange={handleFileSelect}/>
-            <input type="submit" value="Upload File" />
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileSelect}/>
+                <input type="submit" value="Upload File"/>
+            </form>
+            <Stack sx={{ width: '100%', mt:2}} spacing={2}>
+                {isError && <Alert severity={"error"}>{message}</Alert>}
+                {message==="Upload file successfully" && <Alert severity={"success"}>{message}</Alert>}
+            </Stack>
+        </div>
     )
 };
 
